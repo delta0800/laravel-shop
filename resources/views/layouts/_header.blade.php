7347dc6a2e6e23169d074dd70156d89efb9466ed
zzl307 <qq1013446945@126.com>
2019-03-25 17:55:31 +0800

    layout

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                Laravel Shop
+            </a>
+
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Authentication Links -->
+                <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
